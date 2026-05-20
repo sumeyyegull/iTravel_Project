9f01bf7f2c34a9d0a23e4a3a1bde76e3f817692f
sumeyyegull <gulsumeyye872@gmail.com>
2026-05-20 18:28:18 +0300

    Project cleanup and updates

--- app/src/main/java/com/example/itravel/Model/User.java
+++ app/src/main/java/com/example/itravel/Model/User.java
@@ -2,16 +2,15 @@ package com.example.itravel.Model;
 
 public class User {
 
-    public String username, email, phone, image_url;
+    public String username, email, image_url;
 
-    public User(){
-
+    public User() {
+        // Firebase için zorunlu
     }
 
-    public User(String username, String email, String phone, String image_url) {
+    public User(String username, String email, String image_url) {
         this.username = username;
         this.email = email;
-        this.phone = phone;
         this.image_url = image_url;
     }
 }
